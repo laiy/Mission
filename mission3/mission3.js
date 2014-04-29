e6d7d47aa9ae5be15076b874bad4d1156a56fd91
LY <ly.franky@gmail.com>
2014-04-29 14:10:13 +0800

    mission3 from MIAC
    to judge the type exactly

---
+++ mission3/mission3.js
@@ -0,0 +1,20 @@
+util = {}; 
+util.type = function(obj) {
+  var type = Object.prototype.toString.call(obj);
+  var count = 8;
+  var ans = "";
+  while (type[count] != ']') {
+    ans += type[count];
+    count++;
+  }
+  console.log(ans)
+}
+
+util.type({})
+util.type([])
+util.type(null)
+util.type('Hello')
+util.type(34)
+util.type(undefined)
+util.type(function(){})
+util.type(document.body)
